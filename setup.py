e83801c5dff41c006446dec4eb09c977ec434e88
chngyuanlong <chngyuanlong@gmail.com>
2022-02-18 10:08:02 +0800

    before building package

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@
 # -*- coding: utf-8 -*-
 
 from pathlib import Path
-
+from src.config.settings import config
 from setuptools import find_packages, setup
 
 # Package meta-data.
-NAME = 'heart disease classifiction model'
+NAME = 'heart disease classification model'
 DESCRIPTION = "Classifying whether the patient will survive"
 URL = "https://github.com/ChngYuanLongRandy/demo_deploy"
 EMAIL = "chngyuanlong@gmail.com"
 AUTHOR = "RandyChng"
 REQUIRES_PYTHON = "=3.10.0"
+VERSION = config.appConfig.model_version
 
 
 # The rest you shouldn't have to touch too much :)
@@ -19,27 +20,17 @@ REQUIRES_PYTHON = "=3.10.0"
 # Except, perhaps the License and Trove Classifiers!
 # If you do change the License, remember to change the
 # Trove Classifier for that!
-long_description = DESCRIPTION
+with open('README.md', 'r') as f:
+    long_description = f.read()
 
-# Load the package's VERSION file as a dictionary.
-about = {}
-ROOT_DIR = Path(__file__).resolve().parent
-REQUIREMENTS_DIR = ROOT_DIR / 'requirements'
-PACKAGE_DIR = ROOT_DIR / 'regression_model'
-with open(PACKAGE_DIR / "VERSION") as f:
-    _version = f.read().strip()
-    about["__version__"] = _version
-
-
-# What packages are required for this module to be executed?
 def list_reqs(fname="requirements.txt"):
-    with open(REQUIREMENTS_DIR / fname) as fd:
+    with open(fname) as fd:
         return fd.read().splitlines()
 
 # Where the magic happens:
 setup(
     name=NAME,
-    version=about["__version__"],
+    version=VERSION,
     description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -48,22 +39,15 @@ setup(
     python_requires=REQUIRES_PYTHON,
     url=URL,
     packages=find_packages(exclude=("tests",)),
-    package_data={"regression_model": ["VERSION"]},
+    # package_data={"regression_model": ["VERSION"]},
     install_requires=list_reqs(),
-    extras_require={},
+    # extras_require={},
     include_package_data=True,
     license="BSD-3",
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
+        "Programming Language :: Python :: 3.10",
     ],
 )
